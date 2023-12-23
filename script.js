8cf32949f5994fd9c3f5ca56a08c7a36f3f6af29
Claire-ONeill <98348554+Claire-ONeill@users.noreply.github.com>
2023-12-23 00:24:31 -0500

    forwarded navbar to separate html pages under progress

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const navLinks = document.querySelectorAll('.nav-links a');
 
 navLinks.forEach(link => {
-  link.addEventListener('click', function() {
+  link.addEventListener('click', function(event) {
+    // Remove 'active' class from all links
     navLinks.forEach(link => link.classList.remove('active'));
+
+    // Add 'active' class to the clicked link
     this.classList.add('active');
+
+    // Get the href attribute of the clicked link
+    const page = this.getAttribute('href');
+
+    // Navigate to the associated HTML page
+    setTimeout(function() {
+      window.location.href = page;
+    }, 500); // Delay for 500 milliseconds (optional, for better visual feedback)
+    
+    // Prevent the default behavior of the link (preventing the page from reloading)
+    event.preventDefault();
   });
 });
