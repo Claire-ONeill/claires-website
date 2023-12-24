7d43e3ee6a26164dc4b77f7a69009c45c8f20e06
Claire-ONeill <98348554+Claire-ONeill@users.noreply.github.com>
2023-12-24 01:51:36 -0500

    updated contact and basic format of other pages

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
+const form = document.querySelector('form'); 
+const NAME = document.getElementById("name"); 
+const EMAIL = document.getElementById("email"); 
+const PHONE = document.getElementById("phone"); 
+const SUBJECT = document.getElementById("subject"); 
+const BODY = document.getElementById("message"); 
 
-// JavaScript to toggle the active class for the mobile menu
-const menuIcon = document.querySelector('.menu-icon');
-const navLinks = document.querySelector('.nav-links');
+function sendEmail(){
+  const MESSAGE = `Full Name: ${NAME.value}<br> Email: ${EMAIL.value}<br> 
+  Phone: ${PHONE.value}<br> Subject: ${SUBJECT.value}<br> `;
 
-menuIcon.addEventListener('click', function() {
-  navLinks.classList.toggle('active');
-});
-
-// handle form submittion 
-document.getElementById('myForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
-
-  // Get form input values
-  const name = document.getElementById('name').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const message = document.getElementById('message').value.trim();
-
-  // Validation and error message display
-  const nameError = document.getElementById('nameError');
-  const emailError = document.getElementById('emailError');
-  const messageError = document.getElementById('messageError');
-
-  nameError.textContent = '';
-  emailError.textContent = '';
-  messageError.textContent = '';
-
-  if (name === '') {
-      nameError.textContent = 'Please enter your name';
-      return;
-  }
-
-  if (email === '') {
-      emailError.textContent = 'Please enter your email';
-      return;
-  } else {
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailPattern.test(email)) {
-          emailError.textContent = 'Please enter a valid email address';
-          return;
+  Email.send({
+    encryptedToken: "b5bbe703-19cd-4873-b197-350234145cf5", 
+    Host: "smtp.elasticemail.com", 
+    Username: "cconeill@umich.edu", 
+    To: "cconeill@umich.edu", 
+    From: "you@isp.com", 
+    Subject: SUBJECT.value, 
+    Body: MESSAGE
+  }).then(
+    message=>{
+      if (message == "OK"){
+        Swal.fire({
+          title: "Success!",
+          text: "Message sent!",
+          icon: "success"
+        });
       }
+    }
+  )
+}
+function checkInputs(){ 
+  const items = document.querySelectorAll(".item"); 
+  for (const item of items){
+    if (item.value ==""){
+      item.classList.add("error")
+      item.parentElement.classList.add("error"); 
+    }
+    if (items[1].value != ""){
+      checkEmail(); 
+    }
+    items[1].addEventListener("keyup", () =>{
+      checkEmail(); 
+    })
+    item.addEventListener("keyup", ()=> {
+      if (item.value != ""){
+        item.classList.remove("error")
+        item.parentElement.classList.remove("error"); 
+      }
+      else{
+        item.classList.add("error"); 
+        item.parentElement.classList.add("error"); 
+      }
+    })
+  }
+}
+function checkEmail(){
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\/[a-z]{2,3})?$/;
+  if (!EMAIL.value.match(emailRegex)){
+    EMAIL.classList.add("error"); 
+    EMAIL.parentElement.classList.add("error"); 
+  }
+  else{
+    EMAIL.classList.remove("error"); 
+    EMAIL.parentElement.classList.remove("error"); 
+  }
+}
+form.addEventListener("submit", (e) =>{
+  e.preventDefault(); 
+  checkInputs(); 
+
+  if (!NAME.classList.contains("error") && !EMAIL.classList.contains("error") && !PHONE.classList.contains("error")
+    && !SUBJECT.classList.contains("error") && !BODY.classList.contains("error")){
+      sendEmail(); 
+      form.reset(); 
+      return false; 
   }
 
-  if (message === '') {
-      messageError.textContent = 'Please enter your message';
-      return;
-  }
-
-  // If all validations pass
-  alert('Form submitted successfully!');
-});
+  // sendEmail(); 
+}); 
